560a0ab9deaabe2b25f254764f4028ae747252af
411496621 <411496621@qq.com>
2019-03-26 22:57:30 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<img src="./assets/logo.png" alt="" />
+		<HelloWorld />	
+		<hello-world />
+	</div>
+</template>
+
+<script>
+  import HelloWorld from "./components/HelloWorld"
+	export default {
+		components:{
+			HelloWorld
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="item">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+	export default {
+	  data() {
+			return {
+				msg:'Hello World'
+			}
+		},
+	}
+</script>
+
+<style scoped>
+  .item{
+		font-size: 30px;
+		color: red;
+	}
+</style>
